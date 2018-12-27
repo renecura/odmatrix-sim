64d5d7fbefe9a6f7e157f2150dd3c3b5d51ea978
Rodrigo René Cura <rodrigo.renecura@gmail.com>
2018-12-27 19:43:25 -0300

    Better command line arguments handling added

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,52 @@
+-- {-# DeriveDataTypeable #-}
 module Main where
 
   import System.Environment
   import System.Random
   import Numeric.LinearAlgebra (Matrix, assoc, Z, toLists)
   import Control.Monad (join)
+
+  import System.Console.CmdArgs.Implicit
   
   import ODMatrix.Sim.BusState
 
 
 
-  parseArgs :: [String] -> (Int,Int,Int,Int)
-  parseArgs (n:k:s:c:_) = (read n, read k, read s, read c)
-  parseArgs _ = error "Insufficients arguments. usage: main route_size capacity seed number_cases"
+  data SimParams = Params {
+      size :: Int,
+      capacity :: Int,
+      seed :: Int,
+      cases :: Int,
+      lambdas :: Bool
+  } deriving (Show, Data, Typeable)
+
+  params = Params {
+    size = 10 &= help "Size of the route",
+    capacity = 50 &= help "Capacity of the unit",
+    seed = 0 &= help "Seed for the simulation",
+    cases = 10 &= help "Number of cases to simulate",
+    lambdas = False &= help "Indicates if the lists of lambdas gone to be provided manually"
+  }
+
+
+  
+
 
 
   main :: IO ()
   main = do
     putStrLn "# ODMatrix Simulation"
 
-    args <- getArgs
-    let (n, k, seed, cases) = parseArgs args
+    args <- cmdArgs params
+    print args
+
+    let (Params n k seed cases manual_lambdas) = args
         (g1, g2) = split $ mkStdGen seed
 
-    putStrLn "# Generating randoms lambdas..."
+    putStrLn $  if manual_lambdas 
+                then "# Reading lambdas..."
+                else "# Generating randoms lambdas..."
+
     let (bg,ag) = split g1
         bs = take n $ randomRs (0,fromIntegral k) bg
         as = take n $ randomRs (0,fromIntegral k) ag
